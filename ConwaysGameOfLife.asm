9ffbc3460be8698ebe910bc738509d7a72fa34ab
Jonathan Lyttle <myncijub@gmail.com>
2017-11-17 16:45:56 -0600

    More pseudocode

--- ConwaysGameOfLife.asm
+++ ConwaysGameOfLife.asm
@@ -6,8 +6,23 @@
 golArray:	.space		#Make this a dynamic array later.
 gridSize:	.byte	0	#The width or length of the working grid taken from the user in a menu.
 sleepTime:	.byte	0	#The amount of time to wait before displaying the next generation (in ms).
+patternPrompt:	.asciiz	"Choose a pattern (1 for random, 2 for glider gun)"
 
 .text
-InputValidation: #Check if the grid size () and  
-PatternMenu:
-	#if the size of the grid is greater than or equal to 36 we can offer    
+Main:
+
+InputValidation: #Check if the grid size (64, 128, 512, or 1024) and sleep time (0-) are valid and reprompt if not.
+
+PatternMenu: #Choose either the glider gun pattern or a random pattern.
+	li	$v0, 51
+	la	$a0, patternPrompt
+	syscall
+	
+	beq	$a1, -1, Error
+
+Error:
+	li	$v0, 
+
+Exit:
+	li	$v0, 10
+	syscall
