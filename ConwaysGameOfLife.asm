eba258139e9c19b0fbb92caed408602b27ef6e7a
Jonathan Lyttle <myncijub@gmail.com>
2017-11-19 16:04:06 -0600

    The game of life algorithm now works correctly

--- ConwaysGameOfLife.asm
+++ ConwaysGameOfLife.asm
@@ -9,11 +9,12 @@
 #$s2 = chosen pattern
 #$s3 = Black color (blank / dead)
 #$s4 = White color (Living)
-
+#$s5 = Total area of the array (64x64)
 
 .data
-birthsAndDeathsArray:	.space	4096	#An array for tracking births and deaths, the same size as the bitmap display
+birthsAndDeathsArray:	.space	16384	#An array for tracking births and deaths, the same size as the bitmap display (64x64 pixels expressed in words)
 gridSize:		.word	64	#The width or length of the working grid.
+gridArea:		.word	4096
 deadCellColor:		.word	0x000000	#The color chosen for a dead cell
 livingCellColor:	.word	0xffffff	#The color chosen for a living cell
 sleepTime:		.word	0	#The amount of time to wait before displaying the next generation (in ms).
@@ -28,6 +29,7 @@ Main:
 	lw	$s0, gridSize		#Load the grid size into $s0
 	lw	$s3, deadCellColor
 	lw	$s4, livingCellColor
+	lw	$s5, gridArea
 PromptForSleepTime:
 	li	$v0, 51
 	la	$a0, waitTimePrompt	#Ask the user how long to wait until displaying the next generation
@@ -55,28 +57,30 @@ PatternMenu: #Choose either the glider gun pattern or a random pattern.
 
 InitializeArray:	#Create the array with the chosen pattern.
 	beq	$s2, 1, Preset1		#Initialize the glider gun pattern
-	
+
+	Random:	
 	#If the user chose random, we want to intialize the array with a chance of having a living cell. To have more space, we'll choose a third of a chance of spawning.
 	li	$t0, 3	#Initialize $t0 to 3 (the value we compare with).
 	li	$v0, 42	#Choose an int between a range.
 	li	$a1, 9	#Set 9 as the upper bound (0 is the lower bound).
-Preset1:
+	
+	Preset1:
 	#DEBUG: render a single pixel on the grid
-	mulu	$t0, $s0, $s0
 	la	$s1, birthsAndDeathsArray	#Load the birth and death tracking array into $s1		
-	li	$t1, 0			#Get the total size of the space, SRL, and add 32 to get the middle
-	srl	$a0, $t0, 1
+	li	$t1, 0				#Get the total size of the space, SRL, and add 32 to get the middle
+	srl	$a0, $s5, 1
 	addiu	$a0, $a0, 32
 	jal	GetDisplayAddress	#Get the address for that space
 	move	$a0, $v0
-	li	$a1, 0x00ff00
+	addu	$a1, $s4, 0
 	jal	Draw			#Draw the pixel in that space
 	
-	addu	$t2, $s1, 0
-InitializeBirthsAndDeathsArray:
-	sw	$s3, ($t2)		#Store 0 at the current address (for blank)
-	addu	$t2, $s1, 4		#Advance the pointer to the next word
-	bne	$t1, $t0, InitializeBirthsAndDeathsArray	#Loop until the array is filled
+	mul	$t3, $s5, 4		#Area size in words to use with the branch in the loop
+	InitializeBirthsAndDeathsArray:
+	addu	$t2, $s1, $t1		#Get address for current array index into $t2
+	sw	$s3, ($t2)		#Store 0 at the array index (for blank)
+	addiu	$t1, $t1, 4		#Advance the pointer to the next word
+	bne	$t1, $t3, InitializeBirthsAndDeathsArray	#Loop until the array is filled
 
 GameOfLife:
 	li	$t5, 0			#Set $t5 to the current position
@@ -84,75 +88,86 @@ GameOfLife:
 	GOLLoop:	
 		#For each pixel in the display array, run the algorithm
 		addiu	$a0, $t5, 0			#Copy the current position into argument
-		jal	GOLAlgorithm
-		addiu	$a0, $t5, 0
-		jal	GetDisplayAddress
-		lw	$t2, ($v0)			#$t2 = value at current address
+		jal	GOLAlgorithm			#Run the algorithm given position. # neighbors in $v1
+		addiu	$a0, $t5, 0			#Copy current position to argument
+		jal	GetDisplayAddress		#Get address of pixel in $v0
+		lw	$t2, ($v0)			#$t2 = color at pixel's address
 		mul	$t4, $t5, 4			#Convert current position to a word value in $t4
-		addu	$t4, $t4, $s1			#Store the current address in the births and deaths array as $t4
-		beq	$t2, $s3, Dead
+		addu	$t4, $t4, $s1			#Store the base address in the births and deaths array plus the position offset as $t4
+		beq	$t2, $s3, Dead			#If the color we got was black, the current cell is dead
 		Live:					#The current cell is alive if the value isn't blank
 			#if $v1 returned less than 2, set this cell as dead
-			blt	$v1, 2, SetDead
-			bgt	$v1, 3, SetDead
-			b	CheckLoop
+			blt	$v1, 2, SetDead		#Set as dead if there are fewer than 2 neighbors
+			bgt	$v1, 3, SetDead		#Set as dead if there are greater than 3 neighbors
+			j	CheckLoop
 			SetDead:
 				sw	$s3, ($t4)
+				b	CheckLoop
 		Dead:
+			#if $v1 returned 3, set this cell as alive
+			beq	$v1, 3, SetAlive
+			j	CheckLoop
+			SetAlive:
+				sw	$s4, ($t4)
 		
-		CheckLoop:
+		CheckLoop:	#Check if we still need to loop through the rest of the display
 		addiu	$t5, $t5, 1			#Advance current position by 1
-		bgt	$t5, $t0, DisplayGeneration	#If the current position is greater than the total size, start displaying the births and deaths of this generation
+		bgt	$t5, $s5, DisplayGeneration	#If the current position is greater than the total size, start displaying the births and deaths of this generation
 
 	GOLAlgorithm:
-		#$t1 is the absolute position of the pixel, $t0 is the total size of the grid, $v1 is the neighbor counter.
+		#Store return address in the stack
+		addi	$sp, $sp, -4
+		sw	$ra, ($sp)
+	
+		#$t1 is the absolute position of the pixel, $s5 is the total size of the grid, $s0 is width of one line, $v1 is the neighbor counter.
 		li	$v1, 0		#initialize counter to 0
 		move	$t1, $a0	#Move the argument to $t1 to work with
 	
 		#if ($t1 < 64); we're on the top row so move on to checking the middle row
-		blt	$t1, 64, CheckMiddle
+		blt	$t1, $s0, CheckMiddle
 	CheckTop:
-		#if (($t1 % 64) == 0); we're on the left border so don't check top left and move on to the right
-		rem	$t2, $t1, 64	#$t2 = $t1 % 64
-		beq	$t2, $zero, CheckTopRight
+		#if (($t1 % 64) == 0); we're on the left border so don't check top left and move on to the middle
+		rem	$t2, $t1, $s0	#$t2 = $t1 % 64
+		beq	$t2, $zero, CheckTopMiddle
 
 		CheckTopLeft:
 		#if the value at ($t1 - 65) is the color white or green, we have a neighbor and we increment the counter
-		addi	$a0, $t1, -65			#$a0 is the new position to check
+		subu	$a0, $t1, $s0			#$a0 is the new position to check
+		subiu	$a0, $a0, 1
 		jal	GetDisplayAddress		#Calculate the address of the pixel
 		lw	$t2, ($v0)			#Load the value at this location into $t2
-		beq	$t2, 0x000000, CheckTopMiddle	#If the value at $t2 is either black or red, don't increment
-		beq	$t2, 0xff0000, CheckTopMiddle
+		beq	$t2, $s3, CheckTopMiddle	#If the value at $t2 is either black or red, don't increment
+		#beq	$t2, 0xff0000, CheckTopMiddle
 		addiu	$v1, $v1, 1			#Increment the neighbor counter
 	
 		CheckTopMiddle:
-		#if we can check top left, we can also check top middle so we fall through
+		#if we can check top row at all, we can also check top middle
 		#if the value at ($t1 - 64) is the color white or green, we have a neighbor and we increment the counter
-		addi	$a0, $t1, -64			#$a0 is the new position to check
+		subu	$a0, $t1, $s0			#$a0 is the new position to check
 		jal	GetDisplayAddress		#Calculate the address of the pixel
 		lw	$t2, ($v0)			#Load the value at this location into $t2
-		beq	$t2, 0x000000, CheckTopRight	#If the value at $t2 is either black or red, don't increment
-		beq	$t2, 0xff0000, CheckTopRight
+		beq	$t2, $s3, CheckTopRight		#If the value at $t2 is either black or red, don't increment
+		#beq	$t2, 0xff0000, CheckTopRight
 		addiu	$v1, $v1, 1			#Increment the neighbor counter
 
 		CheckTopRight:	
 		#if ((($t1 + 1) % 64) == 0); we're on the right border so don't check the top right and move on to the middle row
-		addi	$t2, $t1, 1	#$t2 = $t1 + 1
-		rem	$t2, $t2, 64	#$t2 = $t2 % 64
+		addiu	$t2, $t1, 1	#$t2 = $t1 + 1
+		rem	$t2, $t2, $s0	#$t2 = $t2 % 64
 		beq	$t2, $zero, CheckMiddle
 	
 		#if the value at ($t1 - 63) is the color white or green, we have a neighbor and we increment the counter
-		addi	$a0, $t1, -63			#$a0 is the new position to check
+		addi	$a0, $s0, -1
+		subu	$a0, $t1, $a0			#$a0 is the new position to check
 		jal	GetDisplayAddress		#Calculate the address of the pixel
 		lw	$t2, ($v0)			#Load the value at this location into $t2
-		beq	$t2, 0x000000, CheckTopRight	#If the value at $t2 is either black or red, don't increment
-		beq	$t2, 0xff0000, CheckTopRight
+		beq	$t2, $s3, CheckMiddle	#If the value at $t2 is either black or red, don't increment
+		#beq	$t2, 0xff0000, CheckTopRight
 		addiu	$v1, $v1, 1			#Increment the neighbor counter
 
 	CheckMiddle:
 		#if (($t1 % 64) == 0); don't check the middle left and move on to the right
-		addi	$t2, $t1, 0	#$t2 = $t1
-		rem	$t2, $t2, 64	#$t2 = $t2 % 64
+		rem	$t2, $t1, $s0	#$t2 = $t1 % 64
 		beq	$t2, $zero, CheckMiddleRight
 	
 		CheckMiddleLeft:
@@ -160,72 +175,75 @@ GameOfLife:
 		addi	$a0, $t1, -1			#$a0 is the new position to check
 		jal	GetDisplayAddress		#Calculate the address of the pixel
 		lw	$t2, ($v0)			#Load the value at this location into $t2
-		beq	$t2, 0x000000, CheckMiddleRight	#If the value at $t2 is either black or red, don't increment
-		beq	$t2, 0xff0000, CheckMiddleRight
+		beq	$t2, $s3, CheckMiddleRight	#If the value at $t2 is either black or red, don't increment
+		#beq	$t2, 0xff0000, CheckMiddleRight
 		addiu	$v1, $v1, 1			#Increment the neighbor counter
 	
 		CheckMiddleRight:
 		#if ((($t1 + 1) % 64) == 0); don't check the middle right and move on to the bottom row
 		addi	$t2, $t1, 1	#$t2 = $t1 + 1
-		rem	$t2, $t2, 64	#$t2 = $t2 % 64
+		rem	$t2, $t2, $s0	#$t2 = $t2 % 64
 		beq	$t2, $zero, CheckBottom
 	
 		#if the value at ($t1 + 1) is the color white or green, we have a neighbor and we increment the counter
 		addi	$a0, $t1, 1			#$a0 is the new position to check
 		jal	GetDisplayAddress		#Calculate the address of the pixel
 		lw	$t2, ($v0)			#Load the value at this location into $t2
-		beq	$t2, 0x000000, CheckBottom	#If the value at $t2 is either black or red, don't increment
-		beq	$t2, 0xff0000, CheckBottom
+		beq	$t2, $s3, CheckBottom	#If the value at $t2 is either black or red, don't increment
+		#beq	$t2, 0xff0000, CheckBottom
 		addiu	$v1, $v1, 1			#Increment the neighbor counter
 	
 	CheckBottom:
-		#if (($t1 + 64) > $t0); don't check the entire bottom row and return
-		addi	$t2, $t1, 64	#$t2 = $t1 + 64
-		bgt	$t2, $t0, Return
+		#if (($t1 + 64) > $s5); don't check the entire bottom row and return
+		addu	$t2, $t1, $s0	#$t2 = $t1 + 64
+		bgt	$t2, $s5, Return
 
 		#If (($t1 % 64) == 0); don't check bottom left and move on to the middle
-		addi	$t2, $t1, 0	#$t2 = $t1 + 64
-		rem	$t2, $t2, 64	#$t2 = $t2 % 64
+		rem	$t2, $t1, $s0	#$t2 = $t1 % 64
 		beq	$t2, $zero, CheckBottomMiddle
 	
 		CheckBottomLeft:
 		#if the value at ($t1 + 63) is the color white or green, we have a neighbor and we increment the counter
-		addi	$a0, $t1, 63			#$a0 is the new position to check
+		addi	$a0, $s0, -1
+		addu	$a0, $a0, $t1			#$a0 is the new position to check
 		jal	GetDisplayAddress		#Calculate the address of the pixel
 		lw	$t2, ($v0)			#Load the value at this location into $t2
-		beq	$t2, 0x000000, CheckBottomMiddle	#If the value at $t2 is either black or red, don't increment
-		beq	$t2, 0xff0000, CheckBottomMiddle
+		beq	$t2, $s3, CheckBottomMiddle	#If the value at $t2 is either black or red, don't increment
+		#beq	$t2, 0xff0000, CheckBottomMiddle
 		addiu	$v1, $v1, 1			#Increment the neighbor counter
 	
 		CheckBottomMiddle:
 		#If we can check the bottom at all we must be able to check under the current position
 		#if the value at ($t1 + 64) is the color white or green, we have a neighbor and we increment the counter
-		addi	$a0, $t1, 64			#$a0 is the new position to check
+		addu	$a0, $t1, $s0			#$a0 is the new position to check
 		jal	GetDisplayAddress		#Calculate the address of the pixel
 		lw	$t2, ($v0)			#Load the value at this location into $t2
-		beq	$t2, 0x000000, CheckBottomRight	#If the value at $t2 is either black or red, don't increment
-		beq	$t2, 0xff0000, CheckBottomRight
+		beq	$t2, $s3, CheckBottomRight	#If the value at $t2 is either black or red, don't increment
+		#beq	$t2, 0xff0000, CheckBottomRight
 		addiu	$v1, $v1, 1			#Increment the neighbor counter
 	
-		#if ((($t1 + 1) % 64) == 0); don't check the bottom right and return
-		addi	$t2, $t1, 1	#$t1 = $a0 + 1
-		rem	$t2, $t2, 64	#t1 = $t1 % 64
-		beq	$t2, $zero, Return
-	
 		CheckBottomRight:
+		#if ((($t1 + 1) % 64) == 0); don't check the bottom right and return
+		addiu	$t2, $t1, 1	#$t1 = $a0 + 1
+		rem	$t2, $t2, $s0	#t1 = $t1 % 64
+		beq	$t2, $zero, Return
+		
 		#if the value at ($t1 + 65) is the color white or green, we have a neighbor and we increment the counter
-		addi	$a0, $t1, 65			#$a0 is the new position to check
+		addiu	$a0, $t1, 1			#$a0 is the new position to check
+		addu	$a0, $a0, $s0
 		jal	GetDisplayAddress		#Calculate the address of the pixel
 		lw	$t2, ($v0)			#Load the value at this location into $t2
-		beq	$t2, 0x000000, CheckBottom	#If the value at $t2 is either black or red, don't increment
-		beq	$t2, 0xff0000, CheckBottom
+		beq	$t2, $s3, Return		#If the value at $t2 is either black or red, don't increment
+		#beq	$t2, 0xff0000, Return
 		addiu	$v1, $v1, 1			#Increment the neighbor counter
 	
 	Return:
 		#v1 returns the number of neighbors
+		lw	$ra, ($sp)
+		addi	$sp, $sp, 4
 		jr	$ra
 
-DisplayGeneration:	#Display the current generation in the bitmap display.
+DisplayGeneration:	#Display the current generation in the bitmap display from the births and deaths array.
 	mulu	$t0, $s0, $s0	#Multiply width by height (same value) to get total size.
 	li	$t1, 0		#Initialize variable for current position.
 DrawLoop:
